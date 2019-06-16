16c3fbf34df3b5dba84a67a09956394790bd23a7
Adi Pandit <adi.oss.pandit@gmail.com>
2019-06-16 09:35:48 -0400

    Some more formatting changes

--- formatSchedule.py
+++ formatSchedule.py
@@ -1,11 +1,11 @@
 import csv
 import datetime
 
+saturday = datetime.date(2019,6,15)
+sunday = saturday + datetime.timedelta(days=1)
 
-saturday = datetime.date(2019,6,8)
-sunday = datetime.date(2019,6,9)
-inputFile = ''
-outputFile = 'june-08-2019-games.csv'
+inputFile = 'week10-games.csv'
+outputFile = 'june-15-2019-games.csv'
 with open(outputFile, mode='w') as weekend_games:
     game_writer = csv.writer(weekend_games, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     game_writer.writerow(['G', 'Grd', 'D','S','Home','Team','Away','Team',
@@ -23,6 +23,6 @@ with open(outputFile, mode='w') as weekend_games:
                 gameday = datetime.datetime.strptime(row["Game Time"], "%Y-%m-%d %H:%M:%S").date()
                 if gameday == saturday or gameday == sunday:
                     game_writer.writerow([row["Gender"],row["Grade"],row["Division"],row["Section"],'NYSA',
-                                          row["Home Team"],row["Away Organization"],row["Away Team"],
-                                          row["Game Time"],row["Field"], row["Referee"], row["Sr. Assistant Ref"],
-                                          row["Jr. Assistant Ref"]])
+                                          row["Home Team"].split(' ',1)[0],row["Away Organization"].split(' ',1)[0],
+                                          row["Away Team"].split(' ',1)[0],row["Game Time"],row["Field"].split(' ',3)[2],
+                                          row["Referee"], row["Sr. Assistant Ref"],row["Jr. Assistant Ref"]])
